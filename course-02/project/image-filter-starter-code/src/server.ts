1351dc63c2ffa895ffe57aac9ecd44318153424f
shicks1080 <stuart.hicks@gmail.com>
2020-04-28 14:40:46 +0200

    Generate the request for a filtered image
    Following the TODO instructions the image url is extracted from the query and there is a check that it exists.
    The url is then passed through the filter.
    The filtered image is then send and deleted from the local system.

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -21,7 +21,6 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // GET /filteredimage?image_url={{URL}}
   // endpoint to filter an image from a public url.
   // IT SHOULD
-  //    1
   //    1. validate the image_url query
   //    2. call filterImageFromURL(image_url) to filter the image
   //    3. send the resulting file in the response
@@ -34,6 +33,23 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   /**************************************************************************** */
 
   //! END @TODO1
+  app.get("/filteredimage", async (req, res) => {
+    
+    // Task 1 - check that there is a query
+    const imageUrl: string = await req.query.image_url
+    if (imageUrl === "") {
+      return res.status(400).send("You need to include a query url.")
+    }
+
+    // Task 2 - pass the valid image's url through the filter provided    
+    const filteredImage = await filterImageFromURL(imageUrl)
+
+    // Task 3 - Send the filtered image in the response
+    res.sendFile(filteredImage, () => {
+      // Task 4 - delete the files saved locally
+      deleteLocalFiles([filteredImage])
+    })   
+  })
   
   // Root Endpoint
   // Displays a simple message to the user
